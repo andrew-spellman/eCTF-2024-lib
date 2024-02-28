350e8e1c269882fdf488ca4589fb6a15d6c96ee6
Andrew Spellman <spellmanandrew@protonmail.com>
2024-02-27 20:52:37 -0600

    PROJECT: Init git in new 2024-ectf

--- src/host_msg.rs
+++ src/host_msg.rs
@@ -1,8 +1,11 @@
 use core::ops::{Deref, DerefMut};
 
-use max78000_hal::debug::{attach_debug, DebugStream};
-use max78000_hal::debug_println;
-use max78000_hal::uart::{BaudRates, CharacterLength, ParityValueSelect, StopBits, UART, UART0};
+use max78000_hal::{
+    debug::attach_debug,
+    debug_println,
+    error::{ErrorKind, Result},
+    uart::{BaudRates, CharacterLength, ParityValueSelect, StopBits, UART, UART0},
+};
 
 struct HostMsg {
     uart: UART<UART0>,
@@ -109,4 +112,75 @@ macro_rules! host_msg {
     (Ack) => {{
         max78000_hal::debug::_print(format_args!("%ack%\n"));
     }};
+}
+
+pub fn receive_msg(prompt: &str, rx_buffer: &mut [u8]) -> Result<usize> {
+    host_msg!(Prompt, "{}", prompt);
+    let mut rx_byte_count = 0;
+    loop {
+        match unsafe { UART_DEBUG.as_mut().unwrap().uart.read_receive_fifo() } {
+            Ok(next_byte) => {
+                if next_byte as char == '\r' || rx_byte_count == rx_buffer.len() {
+                    break;
+                }
+                rx_buffer[rx_byte_count] = next_byte;
+                rx_byte_count += 1;
+            }
+            Err(_) => (), // uart hasn't received any data
+        }
+    }
+    if rx_byte_count == rx_buffer.len() {
+        return Err(ErrorKind::Overflow);
+    }
+    Ok(rx_byte_count)
+}
+
+pub fn parse_args<'a>(rx_buffer: &'a [u8]) -> (Option<&'a str>, Option<&'a str>, Option<&'a str>) {
+    let mut args = (None, None, None);
+    let mut arg_count = 0;
+    let result: core::result::Result<usize, usize> =
+        rx_buffer
+            .iter()
+            .enumerate()
+            .try_fold(0, |start_arg, (i, &byte)| match byte as char {
+                ' ' if start_arg == i => Ok(start_arg + 1),
+                ' ' => {
+                    let arg =
+                        Some(unsafe { core::str::from_utf8_unchecked(&rx_buffer[start_arg..i]) });
+                    match arg_count {
+                        0 => args.0 = arg,
+                        1 => args.1 = arg,
+                        2 => {
+                            args.2 = arg;
+                            return Err(start_arg);
+                        }
+                        _ => unreachable!("branch 2 would have returned err"),
+                    }
+                    arg_count += 1;
+                    Ok(i + 1)
+                }
+                _ => Ok(start_arg),
+            });
+    let last_start_arg = match result {
+        Ok(i) => i,
+        Err(i) => i,
+    };
+    if last_start_arg != rx_buffer.len() {
+        let next_space = rx_buffer[last_start_arg..]
+            .iter()
+            .take_while(|&&byte| byte as char != ' ')
+            .count();
+        let arg = Some(unsafe {
+            core::str::from_utf8_unchecked(&rx_buffer[last_start_arg..last_start_arg + next_space])
+        });
+        match arg_count {
+            0 => args.0 = arg,
+            1 => args.1 = arg,
+            2 => {
+                args.2 = arg;
+            }
+            _ => (),
+        }
+    }
+    args
 }
